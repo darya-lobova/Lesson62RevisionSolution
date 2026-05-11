ebe3e64fc76d97643769febdb7704282427618f7
st <st@A510PC3>
2026-05-11 18:10:35 +0300

    Task02 solved

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -8,6 +8,43 @@
 
 #include "logic.h"
 
+int count_positive_values(int** matrix, int n, int m) {
+	if (matrix == nullptr, n <= 0, m <= 0) {
+		return 0;
+	}
+
+	int count_positive = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (*(*(matrix + i) + j) > 0) {
+				count_positive++;
+			}
+		}
+	}
+	return count_positive;
+
+}
+
 int count_rows_with_more_positive_values(int** matrix, int n, int m) {
-	return 0;
+
+	if (matrix == nullptr, n <= 0, m <= 0) {
+		return 0;
+	}
+
+	int all_elements = n - 1;
+	int count_positive = count_positive_values(matrix, n, m);
+	int count_rows = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+			if (all_elements - count_positive < count_positive) {
+				count_rows++;
+			}
+	}
+
+	return count_rows;
+
 }

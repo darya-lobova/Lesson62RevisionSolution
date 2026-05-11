e2e5646053cf1ef7ee3265a2c4eaa35e42dc1391
st <st@A510PC3>
2026-05-11 18:45:35 +0300

    Task04 developing

--- Task04/logic.cpp
+++ Task04/logic.cpp
@@ -8,6 +8,65 @@
 
 #include "logic.h"
 
+int find_max_element(int** matrix, int n, int m) {
+	int max_value = matrix[0][0];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (*(*(matrix + i) + j) > max_value) {
+				max_value = *(*(matrix + i) + j);
+			}
+		}
+	}
+	return max_value;
+}
+
+int find_min_element(int** matrix, int n, int m) {
+	int min_value = matrix[0][0];
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (*(*(matrix + i) + j) < min_value) {
+				min_value = *(*(matrix + i) + j);
+			}
+		}
+	}
+	return min_value;
+}
+
 int sum_elements_of_columns_with_extreme_value(int** matrix, int n, int m) {
-	return 0;
+
+	if (matrix == nullptr || n <= 0 || m <= 0) {
+		return 0;
+	}
+
+	int max = find_max_element(matrix, n, m);
+	int min = find_min_element(matrix, n, m);
+
+	int total_sum = 0;
+
+	for (int j = 0; j < m; j++)
+	{
+		bool has_extreme = false;
+		for (int i = 0; i < n; i++)
+		{
+			if (matrix[i][j] == max || matrix[i][j] == min) {
+				has_extreme = true;
+				break;
+			}
+		}
+
+		if (has_extreme) {
+			for (int i = 0; i < n; i++)
+			{
+				total_sum += matrix[i][j];
+			}
+		}
+
+	}
+
+	return total_sum;
 }
--- Task04/logic.h
+++ Task04/logic.h
@@ -1,1 +1,3 @@
-﻿int sum_elements_of_columns_with_extreme_value(int** matrix, int n, int m);
+﻿int find_max_element(int** matrix, int n, int m);
+int find_min_element(int** matrix, int n, int m);
+int sum_elements_of_columns_with_extreme_value(int** matrix, int n, int m);
